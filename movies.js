86d9385658521fe3bd8f2d09ca4b4d2ca523e536
brianq-nisra <brian.quinn@nisra.gov.uk>
2023-10-27 12:49:27 +0100

    Add wathced data

--- movies.js
+++ movies.js
@@ -1,461 +1,692 @@
 let movies = {
     "8-bit Christmas": {
         id: 802217,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "A Castle for Christmas": {
         id: 758879,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "A Charlie Brown Christmas": {
         id: 13187,
-        plex: false
+        plex: false,
+        watched: [2020, 2022],
+        added: 2020
     },
     "Scrooge (1951)": {
         id: 13188,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "A Christmas Story": {
         id: 850,
-        plex: true
+        plex: true,
+        watched: [2020, 2022],
+        added: 2020
     },
     "A Christmas Story Christmas": {
         id: 929340,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "A Muppet Family Christmas": {
         id: 13247,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "A Muppets Christmas: Letters To Santa": {
         id: 27190,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "About Fate": {
         id: 828613,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "All I Want For Christmas": {
         id: 13644,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2021
     },
     "Anna and the Apocalypse ": {
         id: 461928,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Arthur Christmas": {
         id: 51052,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "Bell, Book and Candle": {
         id: 2006,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Best. Christmas. Ever!": {
         id: 987490,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2023
     },
     "Black Christmas (1974)": {
         id: 16938,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Black Christmas (2006)": {
         id: 9656,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Black Christmas (2019)": {
         id: 551808,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Carol": {
         id: 258480,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Christmas Holiday": {
         id: 26530,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2022
     },
     "Christmas in Connecticut": {
         id: 13669,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Christmas with the Campbells": {
         id: 934559,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Christmas with the Coopers": {
         id: 333348,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Christmas with the Kranks": {
         id: 13673,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Christmas With You": {
         id: 936960,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Click & Collect": {
         id: 566565,
-        plex: false
+        plex: false,
+        watched: [2021, 2022],
+        added: 2021
     },
     "Deck the Halls": {
         id: 9969,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Die Hard": {
         id: 562,
-        plex: false
+        plex: false,
+        watched: [2020, 2021],
+        added: 2020
     },
     "Dr Seuss' The Grinch (2018)": {
         id: 360920,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Elf": {
         id: 10719,
-        plex: true
+        plex: true,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Emmet Otter's Jugband Christmas": {
         id: 20785,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Falling for Christmas": {
         id: 833097,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Four Christmases": {
         id: 12193,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "Fred Claus": {
         id: 5375,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Game Over": {
         id: 46959,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Guardians of the Galaxy Holiday Special": {
         id: 774752,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Happiest Season": {
         id: 520172,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Holidate": {
         id: 615665,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "Holiday Inn": {
         id: 13485,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Home Alone": {
         id: 771,
-        plex: false
+        plex: false,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Home Alone 2: Lost in New York": {
         id: 772,
-        plex: false
+        plex: false,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Home for the Holidays": {
         id: 9089,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Home Sweet Home Alone": {
         id: 654974,
-        plex: false},
+        plex: false,
+        watched: [2021],
+        added: 2021
+    },
     "How the Grinch Stole Christmas (1966)": {
         id: 13377,
-        plex: false
-        },
+        plex: false,
+        watched: [2020, 2021, 2022],
+        added: 2020
+    },
     "How the Grinch Stole Christmas (2000)": {
         id: 8871,
-        plex: true
+        plex: true,
+        watched: [2020, 2021],
+        added: 2020
     },
     "In the Good Old Summertime": {
         id: 25698,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "It Happened on 5th Avenue": {
         id: 52844,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "It's a Very Merry Muppet Christmas Movie": {
         id: 13352,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2021
     },
     "It's a Wonderful Knife": {
         id: 1113278,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2023
     },
     "It's a Wonderful Life": {
         id: 1585,
-        plex: true
+        plex: true,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Jingle All The Way": {
         id: 9279,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Jingle Jangle": {
         id: 622855,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "John Denver and The Muppets: A Christmas Together": {
         id: 45731,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Kiss Kiss Bang Bang": {
         id: 5236,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Klaus": {
         id: 508965,
-        plex: false
+        plex: false,
+        watched: [2020, 2022],
+        added: 2020
     },
     "Lady Gaga and the Muppets Holiday Spectacular": {
         id: 240000,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2021
     },
     "Last Christmas": {
         id: 549053,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Last Train to Christmas ": {
         id: 887609,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Lego Star Wars Holiday Special": {
         id: 732670,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "Little Women (1933)": {
         id: 39938,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Little Women (1949)": {
         id: 43436,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Little Women (1994)": {
         id: 9587,
-        plex: true
+        plex: true,
+        watched: [2020, 2022],
+        added: 2020
     },
     "Little Women (2019)": {
         id: 331482,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Love Hard": {
         id: 734265,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Meet Me in St Louis": {
         id: 909,
-        plex: true
+        plex: true,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Mickey's Christmas Carol": {
         id: 14813,
-        plex: false
+        plex: false,
+        watched: [2020, 2022],
+        added: 2020
     },
     "Mickey's Once Upon a Christmas": {
         id: 15400,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Miracle on 34th Street (1947)": {
         id: 11881,
-        plex: true
+        plex: true,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "The Miracle on 34th Street (1955)": {
         id: 141871,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Miracle on 34th Street (1959)": {
         id: 765873,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2023
     },
     "Miracle on 34th Street (1973)": {
         id: 252539,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Miracle on 34th Street (1994)": {
         id: 10510,
-        plex: true
+        plex: true,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "Mr Willowby's Christmas Tree": {
         id: 210071,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Natitvity!": {
         id: 49522,
-        plex: false
+        plex: false,
+        watched: [2021, 2022],
+        added: 2021
     },
     "Nativity 2: Danger in the Manger": {
         id: 139380,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Nativity 3: Dude, Where's My Donkey?": {
         id: 295049,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Nativity Rocks!": {
         id: 478157,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Noelle": {
         id: 454467,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "One Magic Christmas": {
         id: 13380,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Pluto's Christmas Tree": {
         id: 61080,
-        plex: false
+        plex: false,
+        watched: [2020, 2022],
+        added: 2020
     },
     "Rare Exports: A Christmas Tale": {
         id: 48395,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2022
     },
     "Remember The Night": {
         id: 29617,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Santa Claus The Movie ": {
         id: 13764,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Scrooge (1970)": {
         id: 13765,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Scrooge: A Christmas Carol (2022)": {
         id: 1001865,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2022
     },
     "Scrooged": {
         id: 9647,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Silent Night (2021)": {
         id: 664574,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2023
     },
     "Silent Night (2023)": {
         id: 891699,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2023
     },
     "Single All the Way": {
         id: 810873,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "Spirited": {
         id: 632856,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Surviving Christmas": {
         id: 15566,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "Tangerine": {
         id: 308084,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2021
     },
     "The Best Man Holiday": {
         id: 146304,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2020
     },
     "The Bishop's Wife": {
         id: 19490,
-        plex: false
+        plex: false,
+        watched: [2021, 2022],
+        added: 2021
     },
     "The Christmas Toy": {
         id: 47670,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2021
     },
     "The Family Stone": {
         id: 9043,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "The Great Santa Claus Switch": {
         id: 210061,
-        plex: true
+        plex: true,
+        watched: [],
+        added: 2021
     },
     "The Holiday": {
         id: 1581,
-        plex: false
+        plex: false,
+        watched: [2020, 2021],
+        added: 2020
     },
     "The Mistle-Tones": {
         id: 149032,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2021
     },
     "The Muppet Christmas Carol": {
         id: 10437,
-        plex: false
+        plex: false,
+        watched: [2020, 2021, 2022],
+        added: 2020
     },
     "The Night Before": {
         id: 296100,
-        plex: false
+        plex: false,
+        watched: [2021],
+        added: 2020
     },
     "The Noel Diary": {
         id: 736918,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "The Polar Express": {
         id: 5255,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "The Preacher's Wife": {
         id: 21539,
-        plex: false
+        plex: false,
+        watched: [2021, 2022],
+        added: 2021
     },
     "The Santa Clause": {
         id: 11395,
-        plex: false
+        plex: false,
+        watched: [2020, 2021],
+        added: 2020
     },
     "The Santa Clause 2": {
         id: 9021,
-        plex: false
+        plex: false,
+        watched: [2020],
+        added: 2020
     },
     "The Santa Clause 3: The Escape Clause": {
         id: 13767,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2020
     },
     "The Shop Around the Corner": {
         id: 20334,
-        plex: true
+        plex: true,
+        watched: [2021, 2022],
+        added: 2021
     },
     "Trading Places": {
         id: 1621,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     },
     "Un conte de Noël (A Christmas Tale)": {
         id: 8892,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Violent Night": {
         id: 899112,
-        plex: true
+        plex: true,
+        watched: [2022],
+        added: 2022
     },
     "What Happens Later": {
         id: 970948,
-        plex: false
+        plex: false,
+        watched: [],
+        added: 2023
     },
     "While You Were Sleeping ": {
         id: 2064,
-        plex: false
+        plex: false,
+        watched: [2020, 2022],
+        added: 2020
     },
     "White Christmas": {
         id: 13368,
-        plex: true
+        plex: true,
+        watched: [2020],
+        added: 2020
     },
     "Your Christmas or Mine": {
         id: 865559,
-        plex: false
+        plex: false,
+        watched: [2022],
+        added: 2022
     },
     "Zoey's Extraordinary Christmas": {
         id: 872130,
-        plex: true
+        plex: true,
+        watched: [2021],
+        added: 2021
     }            
 }
